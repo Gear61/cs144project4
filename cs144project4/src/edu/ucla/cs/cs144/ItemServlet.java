d66b353d6ed026db6627d48dfd0164ef5dfbf53e
Alex Chiou <chessnone@yahoo.com>
2013-11-12 20:38:56 -0800

    Just 4 lines of code.

--- cs144project4/src/edu/ucla/cs/cs144/ItemServlet.java
+++ cs144project4/src/edu/ucla/cs/cs144/ItemServlet.java
@@ -1,18 +1,45 @@
 package edu.ucla.cs.cs144;
 
 import java.io.IOException;
+import java.io.StringReader;
+
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.FactoryConfigurationError;
+import javax.xml.parsers.ParserConfigurationException;
 
-public class ItemServlet extends HttpServlet implements Servlet {
-       
-    public ItemServlet() {}
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
-        // your codes here
-    }
+public class ItemServlet extends HttpServlet implements Servlet
+{
+
+	public ItemServlet()
+	{
+	}
+
+	static final String columnSeparator = "|*|";
+	static DocumentBuilder builder;
+
+	static final String[] typeName =
+	{ "none", "Element", "Attr", "Text", "CDATA", "EntityRef", "Entity", "ProcInstr", "Comment", "Document", "DocType",
+			"DocFragment", "Notation", };
+	
+	public static Document loadXMLFromString(String xml) throws Exception
+	{
+	    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+	    DocumentBuilder builder = factory.newDocumentBuilder();
+	    InputSource is = new InputSource(new StringReader(xml));
+	    return builder.parse(is);
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		
+	}
 }
