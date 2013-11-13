3fdff4d4684a823956500a76e58bf27dad7fda18
Alex Chiou <chessnone@yahoo.com>
2013-11-12 20:21:54 -0800

    'First commit'

---
+++ cs144project4/WebContents/basicSearch.jsp
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Basic Search Results</title>
+</head>
+<body>
+	<FORM METHOD="GET" ENCTYPE="multipart/form-data" ACTION=search>
+	Keyword(s): <INPUT TYPE="text" NAME="q">
+	<INPUT TYPE="hidden" NAME="numResultsToSkip" value="0">
+	<INPUT TYPE="hidden" NAME="numResultsToReturn" value="20">
+	<INPUT TYPE=SUBMIT VALUE="New Keyword Search">
+	</FORM>
+
+	<h1>Here are the results for your query: <%= request.getAttribute("query") %></h1>
+    <%= request.getAttribute("results") %>
+</body>
+</html>
---
+++ cs144project4/src/edu/ucla/cs/cs144/SearchServlet.java
@@ -0,0 +1,48 @@
+package edu.ucla.cs.cs144;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class SearchServlet extends HttpServlet implements Servlet
+{
+
+	public SearchServlet()
+	{
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		// Grab query
+		String query = request.getParameter("q");
+		int numResultsToSkip = Integer.parseInt(request.getParameter("numResultsToSkip"));
+		int numResultsToReturn = Integer.parseInt(request.getParameter("numResultsToReturn"));
+
+		AuctionSearchClient searcher = new AuctionSearchClient();
+		SearchResult[] results = searcher.basicSearch(query, numResultsToSkip, numResultsToReturn);
+
+		try
+		{
+			String searchResults = "";
+
+			for (int i = 0; i < results.length; i++)
+			{
+				searchResults += results[i].getItemId() + ": " + results[i].getName() + "<br>";
+			}
+			
+			request.setAttribute("query", query);
+			request.setAttribute("results", searchResults);
+			request.getRequestDispatcher("/basicSearch.jsp").forward(request, response);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
